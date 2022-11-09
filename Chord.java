1ef35e0b91ecbeef429337bda927e1b8750eef51
tea9296 <tea9296@gmail.com>
2022-11-09 15:52:07 -0800

    correct localhost ip

--- Chord.java
+++ Chord.java
@@ -1,8 +1,7 @@
+import java.io.IOException;
+import java.net.*;
 import java.util.Scanner;
-import java.net.UnknownHostException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-
+import java.util.Enumeration;
 
 
 /**
@@ -22,12 +21,27 @@ public class Chord {
 	private static Helper helpers;
 
 	public static void main (String[] args) {
-		
+
 		helpers = new Helper();
 
-		
+
 		// get current ip address
 		String curIp = null;
+
+		// Used to try InetAddress.getLocalHost().getHostAddress() to get ip address,
+		// but one machine may contain multiple interface and different ip addresses which
+		// have localhost and ip address that cannot connect to WAN (127.0.0.1, 192.168.56.1 ...)
+		// So try to use connection to website to make sure the chose ip address can connect to WAN
+		Socket socket = new Socket();
+		try{
+			socket.connect(new InetSocketAddress("google.com", 80));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		//System.out.println(socket.getLocalAddress());
+		curIp = socket.getLocalAddress().getHostAddress();
+		/*
 		try {
 			curIp = InetAddress.getLocalHost().getHostAddress();
 
@@ -35,43 +49,48 @@ public class Chord {
 
 			e1.printStackTrace();
 		}
-		
+		*/
+
+
+
+
+
 		// create node (ip + port num)
 		node = new Node (Helper.createSocketAddress(curIp+":"+args[0]));
-		
+
 		// determine it's creating new chord or join chord
 		if (args.length == 1) {
 			conn = node.getAddress();
 		}
-		
+
 		// join, contact is another node
 		else if (args.length == 3) {
 			conn = Helper.createSocketAddress(args[1]+":"+args[2]);
 			if (conn == null) {
 				System.out.println("\n\nCan't find the input ip address and port number, exiting.\n\n");
 				System.exit(0);
-			}	
+			}
 		}
-		
+
 		else {
 			System.out.println("\n\nInput format error, exiting.");
 			System.exit(0);
 		}
-		
+
 		// try to join ring from contact node
 		boolean successJoin = node.join(conn);
-		
+
 		// fail to join contact node
 		if (!successJoin) {
 			System.out.println("\n\nFail to connect the node we try to join, exiting.\n\n");
 			System.exit(0);
 		}
-		
+
 		// print join info
 		System.out.println("Join the Chord successfully.");
 		System.out.println("Current IP address: "+curIp);
 		node.printNeighbors();
-		
+
 		// begin to take user input, "info" or "quit"
 		Scanner userinput = new Scanner(System.in);
 		while(true) {
@@ -83,7 +102,7 @@ public class Chord {
 				node.stopAllThreads();
 				System.out.println("Successfully Leaves the chord ring...");
 				System.exit(0);
-				
+
 			}
 			else if (userInput.startsWith("table")) {
 				node.printDataStructure();
